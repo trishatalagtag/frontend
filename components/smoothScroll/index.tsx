329c5070145a3196532948190b7444fac45e87c0
Aidre Cabrera <alcabrera@mcm.edu.ph>
2023-11-16 16:52:39 +0800

    Remove unnecessary code and fix linting issues

--- components/smoothScroll/index.tsx
+++ components/smoothScroll/index.tsx
@@ -4,9 +4,7 @@ import { useEffect } from "react";
 export default function smoothScroll({ children }): any {
   useEffect(() => {
     window.scrollTo(0, 0);
-
     const lenis = new Lenis();
-
     function raf(time: any) {
       lenis.raf(time);
 
